29a23eed1203202c69a85b029a13b510bd385d85
Muhammad Bin Khalid <83428194+Muhammad-Ibn-E-Khalid@users.noreply.github.com>
2024-05-07 17:56:06 +0500

    Update script.js

--- Website/script.js
+++ Website/script.js
@@ -243,15 +243,17 @@
         document.body.classList.remove('no-scroll');
     }
 
-    function setCookie(name, value, days) {
-        var expires = "";
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = "; expires=" + date.toUTCString();
-        }
-        document.cookie = name + "=" + (value || "") + expires + "; path=https://muhammad-ibn-e-khalid.github.io/";
+function setCookie(name, value, days) {
+    var expires = "";
+    var path = "https://muhammad-ibn-e-khalid.github.io/";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
     }
+    document.cookie = name + "=" + (value || "") + expires + "; path=" + path;
+}
+
 
     function getCookie(name) {
         var nameEQ = name + "=";
